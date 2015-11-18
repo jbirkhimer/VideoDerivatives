95f11674b1e88efa9de4ac334c003d4c9a726590
jbirkhimer <jbirkhimer@quotient-inc.com>
2015-11-18 11:20:26 -0500

    Initial Commit

---
+++ src/main/java/edu/si/services/Hello.java
@@ -0,0 +1,10 @@
+package edu.si.services;
+
+/**
+ * An interface for implementing Hello services.
+ */
+public interface Hello {
+
+    String hello();
+	
+}
---
+++ src/test/java/edu/si/services/RouteTest.java
@@ -0,0 +1,27 @@
+package edu.si.services;
+
+import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
+
+import org.junit.Test;
+
+public class RouteTest extends CamelBlueprintTestSupport {
+	
+    @Override
+    protected String getBlueprintDescriptor() {
+        return "/OSGI-INF/blueprint/blueprint.xml";
+    }
+
+    @Test
+    public void testRoute() throws Exception {
+        // the route is timer based, so every 5th second a message is send
+        // we should then expect at least one message
+        getMockEndpoint("mock:result").expectedMinimumMessageCount(1);
+
+        //template.sendBodyAndHeader("direct:start", "si:3275", "CamelFedoraPid", "si:3275");
+        template.sendBodyAndHeader("direct:start", "si:3144", "CamelFedoraPid", "si:3144");
+
+        // assert expectations
+        assertMockEndpointsSatisfied();
+    }
+
+}
